b57b3e9c8f0d27f3a123eb846b4baf653ce89b1d
Allen Fuller <allen@allenfuller.com>
2020-11-22 15:04:43 -0700

    Initial commit

---
+++ startup/server/fixtures.js
@@ -0,0 +1,39 @@
+import seeder from '@cleverbeagle/seeder';
+import { Meteor } from 'meteor/meteor';
+
+seeder(Meteor.users, {
+  seedIfExistingData: true,
+  environments: ['development', 'staging', 'production'],
+  data: {
+    static: [
+      {
+        email: 'admin@admin.com',
+        password: 'password',
+        profile: {
+          name: {
+            first: 'Andy',
+            last: 'Warhol',
+          },
+        },
+        roles: ['admin'],
+      },
+    ],
+    dynamic: {
+      count: 5,
+      seed(iteration, faker) {
+        const userCount = iteration + 1;
+        return {
+          email: `user+${userCount}@test.com`,
+          password: 'password',
+          profile: {
+            name: {
+              first: faker.name.firstName(),
+              last: faker.name.lastName(),
+            },
+          },
+          roles: ['user'],
+        };
+      },
+    },
+  },
+});
