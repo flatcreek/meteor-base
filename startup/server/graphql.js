a33ae12f920459f876e1d8a6a5f333f0312756d0
Allen Fuller <allen@flatcreek.com>
2023-02-09 06:39:21 -0700

    BORKED -- WORK IN PROGRESS
    Updating dependencies and making related code changes. App is currently non-functioning in this branch.

--- startup/server/graphql.js
+++ startup/server/graphql.js
@@ -1,8 +1,14 @@
 import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
 import { WebApp } from 'meteor/webapp';
 import { getUser } from 'meteor/apollo';
+import express from 'express';
+import cors from 'cors';
+import { json } from 'body-parser';
+
 import schema from './api';
 
+const app = express();
 const server = new ApolloServer({
   schema,
   context: async ({ req }) => ({
@@ -11,7 +17,15 @@ const server = new ApolloServer({
   uploads: false,
 });
 
-server.applyMiddleware({
-  app: WebApp.connectHandlers,
-  path: '/graphql',
-});
+await server.start();
+
+app.use('/graphql',
+  cors,
+  json(),
+  expressMiddleware(server, {
+    app: WebApp.connectHandlers,
+    context: async ({ req }) => ({
+      user: await getUser(req.headers.authorization),
+    }),
+  }),
+);
